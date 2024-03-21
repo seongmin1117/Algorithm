10c9fa33be87cfd62300e8b705a02b907cbec578
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-03-21 20:59:33 +0900

    [level 3] Title: 양과 늑대, Time: 0.20 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+class Solution {
+    static int max;
+    static boolean[] visited;
+    public int solution(int[] info, int[][] edges) {
+        int n = info.length;
+        visited = new boolean[n];
+        visited[0] = true;
+        backtrack(1,0,edges,info);
+        return max;
+    }
+    static void backtrack(int sheep, int wolf, int[][] edges, int[] info){
+        if(sheep<=wolf) return;
+        max = (sheep > max ? sheep : max);
+        for(int i=0; i<edges.length; i++){
+            int parent = edges[i][0];
+            int child = edges[i][1];
+            // 부모는 방문했고 자식은 방문하지 않았으면 가능
+            if(visited[parent] && !visited[child]){
+                //양이면 양+1 하고 백트래킹
+                if(info[child]==0){
+                    sheep++;
+                    visited[child] =true;
+                    backtrack(sheep,wolf,edges,info);
+                    visited[child] =false;
+                    sheep--;
+                } else {
+                    wolf++;
+                    visited[child] =true;
+                    backtrack(sheep,wolf,edges,info);
+                    visited[child] =false;
+                    wolf--;
+                }
+            }
+        }
+    }
+}
