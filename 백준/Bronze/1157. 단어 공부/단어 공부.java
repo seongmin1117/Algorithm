443151a0062ca52dc93cfceea3b90abec2ab1494
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-03-28 09:42:25 +0900

    [Bronze I] Title: 단어 공부, Time: 276 ms, Memory: 21032 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,32 +1,35 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String str = scanner.nextLine();
-        str = str.toUpperCase();
-
-        int[] list = new int[26];
-
-        for(int i=0; i<str.length(); i++ ) {
-            char ch = str.charAt(i);
-            list[ch-'A']++;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] used = new int[26]; //알파벳 사용한 수
+        String s = br.readLine().toUpperCase(); //대문자로
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            used[ch-'A']++;
         }
+        
         int max = 0;
-        char result = '?';
-
-        for(char ch='A'; ch<='Z'; ch++) {
-            int count = list[ch-'A'];
-            if(count>max) {
-                max=count;
-                result= ch;
-            } else if( count == max) {
-                result = '?';
+        int max_idx = 0;
+        int count = 1;
+        
+        for(int i=0; i<26; i++){
+            if(used[i]>max) {
+                count=1; //최댓값 갱신하면 카운트 초기화
+                max = used[i];
+                max_idx = i;
+            } else if(used[i]==max){
+                count++;
             }
         }
-
-        System.out.println(result);
-
+        // 최대가 1개가 아니면 ? 출력
+        if(count==1){
+            System.out.println((char) (max_idx+'A'));
+        } else {
+            System.out.println('?');
+        }
     }
-
 }
