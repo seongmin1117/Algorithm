8a7455dff079bebb8bd577f2977afdac8b58921b
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-03-28 10:14:00 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 736 ms, Memory: 45596 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,28 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<String,Integer> map = new HashMap<>();
-        int n  = Integer.parseInt(st.nextToken());
-        for(int i=0; i<n; i++){
+        HashSet<String> set = new HashSet<>();
+        int n = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
-            if(st.nextToken().equals("leave")){
-                map.remove(name);
+            if (st.nextToken().equals("leave")) {
+                set.remove(name);
             } else {
-                map.put(name,1);
+                set.add(name);
             }
         }
-        List<String> answer = new ArrayList<>(map.keySet());
-        answer.sort(Collections.reverseOrder());
-        for (String s : answer) {
-            System.out.println(s);
+        
+        List<String> answer = new ArrayList<>(set);
+        Collections.sort(answer);
+        for (int i = answer.size() - 1; i >= 0; i--) {
+            bw.write(answer.get(i));
+            bw.newLine();
         }
-
+        bw.close();
     }
 }
