b383d783234276437e69cdf16a7a6a21b52050c6
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-03-29 11:10:47 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.java
@@ -13,11 +13,11 @@ public class Main {
         int n0 = Integer.parseInt(br.readLine());
         // f(n) = a1*n + a0 ,  g(n)= c*n ;
         // n0 <= 모든 n에 대하여 f(n)<= g(n) 이면 1  아니면 0
-        // a1<c && a1+a0 <= c*n0
-        if(a1>c || a1*n0 +a0 > c*n0) {
-            System.out.println(0);
-        } else {
+        // a1<c && a1*n0 +a0 <= c*  n0
+        if(a1<=c && a1*n0 +a0 <= c*n0) {
             System.out.println(1);
+        } else {
+            System.out.println(0);
         }
     }
 }
