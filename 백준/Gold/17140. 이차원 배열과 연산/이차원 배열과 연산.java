70b4729a53a6c41226e28c1af8bc7529206a3497
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-04-01 17:06:33 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 220 ms, Memory: 21216 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
@@ -1,97 +1,100 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
 
 public class Main {
-    static int r,c,k,max_r,max_c;
-    static int[][] board;
-    static HashMap<Integer,Integer> map;
-    static PriorityQueue<int[]> pq;
-
-    static void sort_r(){
-        for (int i=0; i<max_r; i++){
-            map.clear();
-            for (int i1 : board[i]) {
-                if (i1 ==0) continue;
-                map.put(i1,map.containsKey(i1) ? (map.get(i1)+1):1);
-            }
-            for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-                pq.add(new int[] {entry.getKey(), entry.getValue()});
-            }
-            int idx =0;
-            int temp[] = new int[100];
-            while (!pq.isEmpty()){
-                int[] cur = pq.poll();
-                temp[idx] = cur[0];
-                temp[idx+1] = cur[1];
-                idx +=2;
-            }
-            for (int l=0; l<100; l++){
-                board[i][l] = temp[l];
-            }
-            max_c = Math.max(idx,max_c);
-        }
-    }
-
-    static void sort_c(){
-        for (int j=0; j<max_c; j++){
-            map.clear();
-            for (int i=0; i<max_r; i++){
-                int i1 = board[i][j];
-                if (i1 ==0) continue;
-                map.put(i1,map.containsKey(i1) ? (map.get(i1)+1):1);
-            }
-            for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-                pq.add(new int[] {entry.getKey(), entry.getValue()});
-            }
-            int idx =0;
-            int[] temp = new int[100];
-            while (!pq.isEmpty()){
-                int[] cur = pq.poll();
-                temp[idx] = cur[0];
-                temp[idx+1] = cur[1];
-                idx +=2;
-            }
-            for (int l=0; l<100; l++){
-                board[l][j] = temp[l];
-            }
-            max_r = Math.max(idx,max_r);
-        }
-
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        r = sc.nextInt()-1;
-        c = sc.nextInt()-1;
-        k = sc.nextInt();
-        board = new int[100][100];
-        map = new HashMap<>();
-        pq = new PriorityQueue<>(((o1, o2) -> {
-            int temp = o1[1]-o2[1];
-            if (temp!=0){
-                return temp;
-            } else {
-                return o1[0]-o2[0];
-            }
-        }));
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
-                board[i][j]= sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] board = new int[101][101];
+        for(int i=1; i<=3; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=3; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        max_c =3;
-        max_r =3;
-        int count = 0;
-        while (board[r][c]!=k){
-            if (count>100) {
-                System.out.println(-1);
+        int n = -1;
+        int maxR = 3;
+        int maxC = 3;
+        while (n++<100){
+            if(board[x][y]==k){
+                System.out.println(n);
                 return;
             }
-            count++;
-            if (max_r >= max_c) {
-                sort_r();
-            } else {
-                sort_c();
+
+            if(maxR>=maxC){
+                for(int r=1; r<=maxR; r++){
+                    TreeMap<Integer,Integer> map = new TreeMap<>(); // 키: 숫자 , 값: 개수
+                    PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> { // 숫자, 개수  ( 개수 우선으로)
+                        int temp = o1[1]-o2[1];
+                        if (temp!=0){
+                            return temp;
+                        } else {
+                            return o1[0]-o2[0];
+                        }
+                    }));
+                    for( int c=1; c<=maxC; c++){
+                        int num = board[r][c];
+                        if(num==0) continue;
+                        map.put(num,map.getOrDefault(num,0)+1);
+                    }
+                    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                        pq.add(new int[] {entry.getKey(), entry.getValue()});
+                    }
+                    int idx = 1;
+                    int[] temp = new int[101];
+                    while (!pq.isEmpty()){
+                        int[] cur = pq.poll();
+                        temp[idx] = cur[0];
+                        temp[idx+1] = cur[1];
+                        idx+=2;
+                    }
+                    for(int l=1; l<=100; l++){
+                        board[r][l] = temp[l];
+                    }
+                    maxC = Math.max(maxC,idx);
+                }
+            } else if(maxC>maxR){
+                for(int c=1; c<=maxC; c++){
+                    TreeMap<Integer,Integer> map = new TreeMap<>(); // 키: 숫자 , 값: 개수
+                    PriorityQueue<int[]> pq = new PriorityQueue<>(((o1, o2) -> { // 숫자, 개수  ( 개수 우선으로)
+                        int temp = o1[1]-o2[1];
+                        if (temp!=0){
+                            return temp;
+                        } else {
+                            return o1[0]-o2[0];
+                        }
+                    }));
+                    for(int r=1; r<=maxR; r++){
+                        int num = board[r][c];
+                        if(num==0) continue;
+                        map.put(num,map.getOrDefault(num,0)+1);
+                    }
+                    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                        pq.add(new int[] {entry.getKey(), entry.getValue()});
+                    }
+                    int idx = 1;
+                    int[] temp = new int[101];
+                    while (!pq.isEmpty()){
+                        int[] cur = pq.poll();
+                        temp[idx] = cur[0];
+                        temp[idx+1] = cur[1];
+                        idx+=2;
+                    }
+                    for(int l=1; l<=100; l++){
+                        board[l][c] = temp[l];
+                    }
+                    maxR = Math.max(maxR,idx);
+                }
             }
         }
-        System.out.println(count);
+        System.out.println(-1);
     }
 }
