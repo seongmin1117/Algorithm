c1d74b36ac79b54af87b55ffff9ad6bb69207275
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2024-04-04 10:15:18 +0900

    [Gold IV] Title: 세 수의 합, Time: 420 ms, Memory: 57888 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -21,7 +21,6 @@ public class Main {
         O(N^2logN)까지 가능하다.
         두 수의 합을 우선 구한 뒤 배열에 저장 O(N^2),  twoSum[i] + a[i] 로 세 수의 합을 구한다.
         세수의 합을 바로 HashSet에서 검색 O(1) 있으면 출력하고 종료
-        TreeSet이 메모리를 많이 차지해서 ? 메모리 초과 ?
      */
 
 
@@ -29,7 +28,7 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         TreeSet<Integer> one = new TreeSet<>(((o1, o2) -> o2-o1));
-        TreeSet<Integer> twoSum = new TreeSet<>(((o1, o2) -> o2-o1));
+        HashSet<Integer> twoSum = new HashSet<>();
         TreeSet<Integer> asc = new TreeSet<>();
         for (int i=0; i<n; i++){
             int num = Integer.parseInt(br.readLine());
@@ -41,7 +40,6 @@ public class Main {
                 twoSum.add(i+j);
             }
         }
-
         for (Integer i : one) {
             for(Integer j : asc){
                 if (twoSum.contains(i-j)){
