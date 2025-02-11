ca8828a693f34cc5a6271476941266a0e1070c2c
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2025-02-11 11:23:19 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,30 +1,17 @@
 class Solution {
     
     public String solution(String s) {
-        return convertString(s);
-    }
-    
-    private static String convertString(String input) {
-        StringBuilder result = new StringBuilder();
-        String[] words = input.split(" ",-1);
-        for (int i = 0; i < words.length; i++) {
-            result.append(convertWord(words[i]));
-            if (i < words.length - 1) {
-                result.append(" ");
+        char[] charArray = s.toUpperCase().toCharArray();
+        int idx = 0;
+        for (int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+            idx = (c == ' ') ? 0 : idx+1;
+            if (idx%2 == 0){
+                charArray[i] = Character.toLowerCase(c);
             }
         }
-        return result.toString();
+        return String.valueOf(charArray);
     }
-
-    private static String convertWord(String word) {
-        StringBuilder converted = new StringBuilder();
-        for (int i = 0; i < word.length(); i++) {
-            converted.append(convertChar(i, word.charAt(i)));
-        }
-        return converted.toString();
-    }
-
-    private static char convertChar(int i, char c) {
-        return (i % 2 == 0) ? Character.toUpperCase(c) : Character.toLowerCase(c);
-    }
+    
+    
 }
