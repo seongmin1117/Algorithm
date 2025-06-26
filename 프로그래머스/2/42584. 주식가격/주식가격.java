e88cc624fa9a87347e3f8d20a812621be5d3de03
seongmin1117 <128121051+seongmin1117@users.noreply.github.com>
2025-06-26 22:43:11 +0900

    [level 2] Title: 주식가격, Time: 40.11 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int n = prices.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                int top = stack.pop();
+                answer[top] = i - top;
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            int top = stack.pop();
+            answer[top] = n - 1 - top;
+        }
+
+        return answer;
+    }
+}
